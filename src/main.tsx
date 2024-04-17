b215543c41f85f9c979a5dc37213225de3f79db7
jRydell <johannilsrydell89@gmail.com>
2024-04-17 14:41:01 +0200

    cancel

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import Root, {
 } from "./routes/root";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
